9f6fb5b577c0e0549bee153f125a2de531dd4b9d
app <app@192.168.1.10>
2025-06-12 21:58:56 +0500

    chore: updated config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,10 @@ const config = {
   apiBaseUrl:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000"
-      : "https://fragmentsbackend.mernsol.com",
+      : "https://api.fragmenttrails.com",
   homePageURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000"
-      : "https://fragments-app-landingpage-fe.netlify.app/",
+      : "https://fragmenttrails.com",
 };
 export default config;
